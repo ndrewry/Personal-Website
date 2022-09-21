0e7f26a5ed418918fba740d8071e482da182a1d6
Nicole Drewry <nicoledrewry0@gmail.com>
2022-09-20 23:28:12 -0400

    added navigation icons

--- my-app/src/components/nav/Nav.jsx
+++ my-app/src/components/nav/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './nav.css'
+import {AiFillHome} from 'react-icons/ai'
+import {IoMdContact} from 'react-icons/io'
+import {FaUserAlt} from 'react-icons/fa'
+import {FaThList} from 'react-icons/fa'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><AiFillHome/></a>
+      <a href="#contact"><IoMdContact/></a>
+      <a href="#about"><FaUserAlt/></a>
+      <a href="#experience"><FaThList/></a>
+    </nav>
   )
 }
 
